6d45aed525de5044efe6838b1a5883ae7304d592
Ilya Doroshenko <ilya.doroshenko@kdab.com>
2025-04-28 21:58:49 +0200

    card shop prototype

--- scripts/card.gd
+++ scripts/card.gd
@@ -4,7 +4,13 @@ extends Node
 signal mouse_enter
 signal mouse_exit
 
+@export var collision_mask : int = 0
+
 func _ready() -> void:
+	var area :Area2D = $Area2D
+	area.collision_mask = 1 << collision_mask
+	area.collision_layer = 1 << collision_mask
+	
 	var parent = get_parent()
 	if parent is CardManager:
 		parent.connect_card(self)
