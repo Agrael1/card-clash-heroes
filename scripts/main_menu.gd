a18a9d0edd3c74a356d0fd4d91c8ec33f3e327eb
Ilya Doroshenko <ilya.doroshenko@kdab.com>
2025-05-04 14:43:30 +0200

    main menu + multiplayer connection

---
+++ scripts/main_menu.gd
@@ -0,0 +1,34 @@
+extends Control
+
+var main_scene : PackedScene = preload("res://scenes/main.tscn")
+
+@onready var host : Button = $MenuContainer/MarginContainer/VBoxContainer/Host
+@onready var join : Button = $MenuContainer/MarginContainer/VBoxContainer/Join
+@onready var exit : Button = $MenuContainer/MarginContainer/VBoxContainer/Exit
+@onready var menu : Control = $MenuContainer
+
+@export var port : int = 123
+@export var address : String = "localhost"
+
+var peer = ENetMultiplayerPeer.new()
+
+func load_main_scene() -> void:
+	menu.visible = false
+	add_child(main_scene.instantiate())
+
+func _on_join_pressed() -> void:
+	peer.create_client(address, port)
+	multiplayer.multiplayer_peer = peer
+	load_main_scene()
+
+func _on_host_pressed() -> void:
+	peer.create_server(port, 1)
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(on_peer_connected)
+	load_main_scene()
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+func on_peer_connected(peer_id:int)->void:
+	print("successfuly connected")
