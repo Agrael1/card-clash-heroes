154e6f510ab5bbb8f23f5cc01080facca4a3238f
Ilya Doroshenko <ilya.doroshenko@kdab.com>
2025-05-16 22:09:21 +0200

    gold can be set prior to battle

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,6 +1,8 @@
 class_name MainMenu
 extends Control
 
+signal sync_from_host
+
 var main_scene : PackedScene = preload("res://scenes/main.tscn")
 
 @onready var menu : Control = $MenuContainer
@@ -18,7 +20,9 @@ var main_scene : PackedScene = preload("res://scenes/main.tscn")
 @onready var battle_bg_texture_rect: TextureRect = $BattleBGTextureRect
 
 @onready var port_box : RichTextLabel = $MenuContainer/MarginContainer/HostSubmenu/VBoxContainer/HBoxContainer/RichTextLabel
+@onready var gold_box : RaceSelect = $MenuContainer/MarginContainer/HostSubmenu/RaceSelect2
 var race_box : RaceSelect
+var gold : int = 50
 
 var main_scene_instance : Main
 
@@ -36,6 +40,7 @@ func load_main_scene() -> void:
 	xscene.race = race_box.pick_race()
 	
 	add_child(xscene)
+	xscene.shop.gold = gold
 	main_scene_instance = xscene
 
 func _on_join_pressed() -> void:
@@ -46,6 +51,19 @@ func _on_join_pressed() -> void:
 	race_box = $MenuContainer/MarginContainer/JoinSubmenu/RaceSelect
 
 func host_connect(_peer_id:int):
+	var gvalue = gold_box.pick_race()
+	if gvalue == gold_box.races[0]:
+		gold = 50
+	elif gvalue == gold_box.races[1]:
+		gold = 100
+	elif gvalue == gold_box.races[2]:
+		gold = 150
+	sync_gold.rpc(gold)
+	
+	load_main_scene()
+
+func join_connect():
+	await sync_from_host
 	load_main_scene()
 
 func _on_host_pressed() -> void:
@@ -59,6 +77,11 @@ func _on_host_pressed() -> void:
 		host_connect
 	)
 
+# Called only from host to peer
+@rpc("call_remote", "reliable")
+func sync_gold(in_gold:int):
+	gold = in_gold
+	sync_from_host.emit()
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
@@ -101,13 +124,13 @@ func _on_join_back_pressed() -> void:
 func _on_join_oid_pressed() -> void:
 	if Multiplayer.join(join_oid.text):
 		multiplayer.connected_to_server.connect(
-			load_main_scene
+			join_connect
 		)
 
 func _on_text_edit_text_submitted(new_text: String) -> void:
 	if Multiplayer.join(new_text):
 		multiplayer.connected_to_server.connect(
-			load_main_scene
+			join_connect
 		)
 
 func _on_copy_port_pressed() -> void:
